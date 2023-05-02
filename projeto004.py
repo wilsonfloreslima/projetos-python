121f3233159d7f9e1a5f1359295701bef3027df4
Wilson Flores Lima <110207192+wilsonfloreslima@users.noreply.github.com>
2023-05-02 13:15:36 -0300

    criado projeto004

---
+++ projeto004.py
@@ -0,0 +1,29 @@
+from time import sleep
+#declaração de variáveis
+pessoa = dict()
+dados = list()
+while True:
+    #armazenando dados em dicionários
+    pessoa.clear()
+    print('--'*8)
+    pessoa['nome'] = str(input('Nome: ')).strip()
+    pessoa['idade'] = int(input('Idade: '))
+    pessoa['sexo'] = str(input('Sexo[m/f]: ')).strip().upper()[0]
+    #copiando dicionário para dentro de uma lista
+    dados.append(pessoa.copy())
+    resp = str(input('Cadastrar mais pessoas?[s/n] ')).upper().strip()[0]
+    while resp not in 'SN':
+        resp = str(input('DADOS INVÁLIDOS! Cadastrar mais pessoas?[s/n] ')).upper().strip()[0]
+    if resp == 'N':
+        break
+print('--'*15)
+print('ORGANIZANDO DADOS, AGUARDE...')
+sleep(1)
+print('--'*15)
+#mostrando lista com seus dicionários na tela
+#cada indice da lista será um dicionário
+print(f'{"NOME":5}\t{"IDADE":<8}\t\t{"SEXO":>2}')
+print('--'*15)
+for p in dados:
+    print(f'{p["nome"]:<5}\t{p["idade"]:<8}\t\t{p["sexo"]:>2}')
+print('--'*15)
