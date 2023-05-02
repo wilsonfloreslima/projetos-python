c9c954ab25fa14006478647d01a0a0047ac3ee6a
Wilson Flores Lima <110207192+wilsonfloreslima@users.noreply.github.com>
2023-05-02 13:57:22 -0300

    Update projeto004.py

--- projeto004.py
+++ projeto004.py
@@ -2,15 +2,25 @@ from time import sleep
 #declaração de variáveis
 pessoa = dict()
 dados = list()
+total = tothomem = totmulher = totidade = 0
 while True:
     #armazenando dados em dicionários
     pessoa.clear()
     print('--'*8)
     pessoa['nome'] = str(input('Nome: ')).strip()
     pessoa['idade'] = int(input('Idade: '))
+    totidade += pessoa['idade']
     pessoa['sexo'] = str(input('Sexo[m/f]: ')).strip().upper()[0]
+    #contador de homens
+    if pessoa['sexo'] == 'M':
+        tothomem += 1
+    #contador de mulheres
+    if pessoa['sexo'] == 'F':
+        totmulher += 1
     #copiando dicionário para dentro de uma lista
     dados.append(pessoa.copy())
+    #pessoas cadastradas
+    total += 1
     resp = str(input('Cadastrar mais pessoas?[s/n] ')).upper().strip()[0]
     while resp not in 'SN':
         resp = str(input('DADOS INVÁLIDOS! Cadastrar mais pessoas?[s/n] ')).upper().strip()[0]
@@ -27,3 +37,7 @@ print('--'*15)
 for p in dados:
     print(f'{p["nome"]:<5}\t{p["idade"]:<8}\t\t{p["sexo"]:>2}')
 print('--'*15)
+print(f'Pessoas cadastradas: {total}')
+print(f'Média de idade do grupo: {(totidade/total)}')
+print(f'Mulheres cadastradas: {totmulher}')
+print(f'Homens cadastrados: {tothomem}')
