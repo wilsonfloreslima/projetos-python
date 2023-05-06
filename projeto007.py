496c27ae5482b7a46f3604e36cb10f744ab53fc5
Wilson Flores Lima <110207192+wilsonfloreslima@users.noreply.github.com>
2023-05-06 00:24:31 -0300

    Create projeto007.py

---
+++ projeto007.py
@@ -0,0 +1,6 @@
+from time import sleep
+#programna principal
+nome = str(input('Qual seu nome? '))
+idade = int(input('Qual sua idade? '))
+sexo = str(input('Sexo?[m/f] '))
+pol = int(input('Orientação politica?\n1] Direita\n2] Esquerda\n3] Nenhum\n: '))
